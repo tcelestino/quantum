f12a3c4a859aaa6a23d80e93bf9f001271db0eac
alan-oliv <alanbruno@gmail.com>
2019-04-10 15:27:31 -0300

    new image

--- stories/Alert/Alert.story.jsx
+++ stories/Alert/Alert.story.jsx
@@ -6,23 +6,12 @@ import { Alert } from '../../components';
 const description = `Alerts are used for items that need to be labeled,
 categorized, or organized using keywords that describe them.`;
 
-const sampleChildren = (
-  <span>
-    This is a <strong>sample</strong> message, a much bigger text could fit in
-    here
-    <br />
-    and a buch of other lines too, just to demonstrate
-    <br />
-    how this component behaves ❤
-  </span>
-);
-
 storiesOf('Alert', module).add('Alert', () => (
   <AutoExample
     description={description}
     component={Alert}
     componentProps={{
-      children: sampleChildren,
+      children: `This is awesome!`,
       onClose: () => {},
       icon: 'info',
     }}
--- stories/Introduction/Introduction.story.jsx
+++ stories/Introduction/Introduction.story.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Heading } from '@catho/quantum-storybook-ui';
+import { StoryContainer, Title } from '@catho/quantum-storybook-ui';
+import styled, { css } from 'styled-components';
 import Logo from '../../.storybook/static/logo.svg';
+import Background from '../../.storybook/static/getting-started-bg.png';
 import Introduction from './Introduction';
+
+const StyledTitle = styled(Title)`
+  font-size: 50px;
+`;
+
+const HeadingWrapper = styled.div`
+  position: relative;
+  background-image: url(${Background});
+  background-color: #f3f3f5;
+  padding: 45px 0 !important;
+  background-repeat: no-repeat;
+`;
+
+const StyledP = styled.p`
+  font-size: 17px;
+  letter-spacing: initial;
+  line-height: initial;
+`;
+
+const Image = styled.img`
+  margin-right: 20px;
+`;
+
+const Heading = ({ name, title, image, children }) => (
+  <HeadingWrapper>
+    <StoryContainer>
+      <StyledTitle>
+        {image && <Image alt={title} src={image} width="50" height="50" />}
+        {title || name}
+      </StyledTitle>
+      <StyledP>{children}</StyledP>
+      {name && <ViewOnRemote name={name} />}
+    </StoryContainer>
+  </HeadingWrapper>
+);
 
 storiesOf('Introduction', module).add('Getting started', () => (
   <>
-    <Heading title="Quantum" image={Logo}>
+    <Heading title="Quantum" className="teste">
       CSS in JS based reusable components, are the core of Quantum
       design-system: a library for developing consistent UI/UX at Catho.
     </Heading>
