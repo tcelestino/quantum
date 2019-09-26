a1a0bbc574a2e841890180b6806b862e3626cb2a
Tiago Celestino <tcelestino@gmail.com>
2019-09-26 12:16:50 -0300

    fix(IconFont.jsx) avoid to warning about the matching HTML elements on SSR

--- components/GlobalStyle/sub-components/IconFont.jsx
+++ components/GlobalStyle/sub-components/IconFont.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createGlobalStyle } from 'styled-components';
 
-const IconFont =
-  typeof window === 'undefined'
-    ? () => (
-        <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons&display=block"
-          rel="stylesheet"
-        />
-      )
-    : createGlobalStyle`
+const isWindow = () => {
+  console.log('pegou');
+  useEffect(() => typeof window === 'undefined');
+};
+
+const IconFont = isWindow()
+  ? () => (
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons&display=block"
+        rel="stylesheet"
+      />
+    )
+  : createGlobalStyle`
     @import url('https://fonts.googleapis.com/icon?family=Material+Icons&display=block');
   `;
 
