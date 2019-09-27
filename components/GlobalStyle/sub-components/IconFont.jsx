1e0514f2ffc22306075fb63f835586acf0b8ebce
Tiago Celestino <tcelestino@gmail.com>
2019-09-27 12:07:02 -0300

    feat(IconFont.jsx): use to React Hooks

--- components/GlobalStyle/sub-components/IconFont.jsx
+++ components/GlobalStyle/sub-components/IconFont.jsx
@@ -1,20 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createGlobalStyle } from 'styled-components';
 
-const isWindow = () => {
-  console.log('pegou');
-  useEffect(() => typeof window === 'undefined');
+const hasWindow = win => typeof win === 'undefined';
+
+// custom hooks
+const useWindow = () => {
+  const [win, setWindow] = useState(window);
+
+  useEffect(() => {
+    setWindow(window);
+  });
+
+  return hasWindow(win);
 };
 
-const IconFont = isWindow()
-  ? () => (
-      <link
-        href="https://fonts.googleapis.com/icon?family=Material+Icons&display=block"
-        rel="stylesheet"
-      />
-    )
-  : createGlobalStyle`
-    @import url('https://fonts.googleapis.com/icon?family=Material+Icons&display=block');
-  `;
+const IconFont = () => {
+  const isSSR = useWindow();
+
+  return isSSR ? (
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons&display=block"
+      rel="stylesheet"
+    />
+  ) : (
+    createGlobalStyle`@import url('https://fonts.googleapis.com/icon?family=Material+Icons&display=block');`
+  );
+};
 
 export default IconFont;
